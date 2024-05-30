03faf5cf109c25614c95e1be177067de634e92b0
RodLopezDev <rodrigolopezdev@gmail.com>
2024-05-30 15:23:01 -0500

    fix: :bug: change CRLF to LF in project

--- ms-antifraud/src/app.module.ts
+++ ms-antifraud/src/app.module.ts
@@ -1,9 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        () => {
+          Logger.log(`KAFKA_HOST: ${process.env.KAFKA_HOST}`);
+          Logger.log(`KAFKA_PORT: ${process.env.KAFKA_PORT}`);
+          return {
+            kafka: {
+              host: String(process.env.KAFKA_HOST) || '',
+              port: Number(process.env.KAFKA_PORT) || 9092,
+            },
+          };
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
